0ddf80228cd9efe7166e2a60658dddabeac2574d
Ashutosh Shrivastava <ashutoshmishra422@gmail.com>
2024-10-21 00:08:14 +0100

    handled api error

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,8 +1,8 @@
 "use client";
-import { Button, TextArea, TextField } from "@radix-ui/themes";
+import { Button, Callout, TextArea, TextField } from "@radix-ui/themes";
 import axios from "axios";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
 interface IssueForm {
@@ -13,18 +13,30 @@ interface IssueForm {
 const NewIssuePage = () => {
   const { register, handleSubmit } = useForm<IssueForm>();
   const router = useRouter();
+  const [error, setError] = useState("");
   return (
-    <form
-      className="max-w-xl space-y-3"
-      onSubmit={handleSubmit(async (data) => {
-        await axios.post("/api/issues", data);
-        router.push("/issues");
-      })}
-    >
-      <TextField.Root placeholder="Enter a Title" {...register("title")} />
-      <TextArea placeholder="Description" {...register("description")} />
-      <Button>Submit New Issue</Button>
-    </form>
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root className="mb-5" color="red">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className="space-y-3"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/issues", data);
+            router.push("/issues");
+          } catch (error) {
+            setError("Something Went Wrong");
+          }
+        })}
+      >
+        <TextField.Root placeholder="Enter a Title" {...register("title")} />
+        <TextArea placeholder="Description" {...register("description")} />
+        <Button>Submit New Issue</Button>
+      </form>
+    </div>
   );
 };
 
