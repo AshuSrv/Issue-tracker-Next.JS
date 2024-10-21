48d1a8871427b4e2845567959848cd4d6409c293
Ashutosh Shrivastava <ashutoshmishra422@gmail.com>
2024-10-21 09:27:35 +0100

    added skeleton

--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,15 +1,12 @@
-import prisma from "@/prisma/client";
-import { Button, Table } from "@radix-ui/themes";
-import Link from "next/link";
+import { Table } from "@radix-ui/themes";
 import React from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 import IssueStatusBadge from "../components/IssueStatusBadge";
-import delay from "delay";
 import IssuesAction from "./IssuesAction";
 
-const IssuesPage = async () => {
-  const issues = await prisma.issue.findMany();
-  await delay(2000);
-
+const LoadingIssuesPage = () => {
+  const issues = [1, 2, 3, 4, 5, 6, 7, 8];
   return (
     <div>
       <IssuesAction />
@@ -27,20 +24,20 @@ const IssuesPage = async () => {
         </Table.Header>
         <Table.Body>
           {issues.map((issue) => (
-            <Table.Row key={issue.id}>
+            <Table.Row key={issue}>
               <Table.Cell>
-                {issue.title}
+                <Skeleton />
                 <div className="block md:hidden">
-                  <IssueStatusBadge status={issue.status} />
+                  <Skeleton />
                 </div>
                 {/* When table is shrunk we need it to behave as a block element (e.g., in a non-table layout), */}
               </Table.Cell>
               <Table.Cell className="hidden md:table-cell">
                 {/* If the element needs to be displayed as a table cell on bigger screens */}
-                <IssueStatusBadge status={issue.status} />
+                <Skeleton />
               </Table.Cell>
               <Table.Cell className="hidden md:table-cell">
-                {issue.createdAt.toDateString()}
+                <Skeleton />
               </Table.Cell>
             </Table.Row>
           ))}
@@ -50,4 +47,4 @@ const IssuesPage = async () => {
   );
 };
 
-export default IssuesPage;
+export default LoadingIssuesPage;
