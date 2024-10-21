4f8b2bc43ebea6bb3925c9fc3a7b1697afd3cccc
Ashutosh Shrivastava <ashutoshmishra422@gmail.com>
2024-10-22 00:25:34 +0100

    edit issue page created

---
+++ app/issues/_components/IssueForm.tsx
@@ -0,0 +1,75 @@
+"use client";
+import { Button, Callout, Text, TextArea, TextField } from "@radix-ui/themes";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/ValiadtionSchema";
+import { z } from "zod";
+import { Spinner, ErrorMessage } from "@/app/components";
+import { Issue } from "@prisma/client";
+
+type IssueFormType = z.infer<typeof createIssueSchema>;
+
+interface props {
+  issue?: Issue;
+}
+
+const IssueForm = ({ issue }: props) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueFormType>({
+    resolver: zodResolver(createIssueSchema),
+  });
+
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+  const [error, setError] = useState("");
+
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      setIsLoading(true);
+      await axios.post("/api/issues", data);
+      router.push("/issues");
+    } catch (error) {
+      setIsLoading(false);
+      setError("Something Went Wrong");
+    }
+  });
+
+  return (
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root className="mb-5" color="red">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form className="space-y-3" onSubmit={onSubmit}>
+        <TextField.Root
+          placeholder="Enter a Title"
+          defaultValue={issue?.title}
+          {...register("title")}
+        />
+        {errors.title && <ErrorMessage>{errors?.title?.message}</ErrorMessage>}
+
+        <TextArea
+          placeholder="Description"
+          defaultValue={issue?.description}
+          {...register("description")}
+        />
+        {errors.description && (
+          <ErrorMessage>{errors?.description?.message}</ErrorMessage>
+        )}
+
+        <Button disabled={isLoading}>
+          Submit New Issue {isLoading && <Spinner />}
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default IssueForm;
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,62 +1,8 @@
-"use client";
-import { Button, Callout, Text, TextArea, TextField } from "@radix-ui/themes";
-import axios from "axios";
-import { useRouter } from "next/navigation";
-import React, { useState } from "react";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { createIssueSchema } from "@/app/ValiadtionSchema";
-import { z } from "zod";
-import { Spinner, ErrorMessage } from "@/app/components";
-
-type IssueForm = z.infer<typeof createIssueSchema>;
+import React from "react";
+import IssueForm from "../_components/IssueForm";
 
 const NewIssuePage = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<IssueForm>({
-    resolver: zodResolver(createIssueSchema),
-  });
-
-  const [isLoading, setIsLoading] = useState(false);
-  const router = useRouter();
-  const [error, setError] = useState("");
-
-  const onSubmit = handleSubmit(async (data) => {
-    try {
-      setIsLoading(true);
-      await axios.post("/api/issues", data);
-      router.push("/issues");
-    } catch (error) {
-      setIsLoading(false);
-      setError("Something Went Wrong");
-    }
-  });
-
-  return (
-    <div className="max-w-xl">
-      {error && (
-        <Callout.Root className="mb-5" color="red">
-          <Callout.Text>{error}</Callout.Text>
-        </Callout.Root>
-      )}
-      <form className="space-y-3" onSubmit={onSubmit}>
-        <TextField.Root placeholder="Enter a Title" {...register("title")} />
-        {errors.title && <ErrorMessage>{errors?.title?.message}</ErrorMessage>}
-
-        <TextArea placeholder="Description" {...register("description")} />
-        {errors.description && (
-          <ErrorMessage>{errors?.description?.message}</ErrorMessage>
-        )}
-
-        <Button disabled={isLoading}>
-          Submit New Issue {isLoading && <Spinner />}
-        </Button>
-      </form>
-    </div>
-  );
+  return <IssueForm />;
 };
 
 export default NewIssuePage;
